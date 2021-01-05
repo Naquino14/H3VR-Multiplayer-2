2cf77cd509182a15532e3873c88ef670f594402f
Naquino14 <naquino14@outlook.com>
2021-01-05 09:03:44 -0500

    added discordRP support?

---
+++ Assembely-CSharp/DRP/DRP.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using DiscordPresence;
+
+namespace H3VRMultiplayer2.DRP
+{
+    class DRP : MonoBehaviour
+    {
+        #pragma warning disable IDE0044
+
+        public static DRP _DRP;
+        [SerializeField]
+        private string detail = "Test Detail";
+        [SerializeField]
+        private string state = "Test State";
+        private long elapsedTime;
+
+        private void Start()
+        {
+            GameObject DRPContainer = new GameObject();
+            DRPContainer.AddComponent<DRP>();
+            _DRP = DRPContainer.GetComponent<DRP>();
+            Instantiate(DRPContainer);
+
+            elapsedTime = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            PresenceManager.UpdatePresence(detail, state, elapsedTime);
+        }
+        public static void UpdateState(string _detail)
+        {
+            Debug.Log("Updating DRP");
+            PresenceManager.UpdatePresence(_detail);
+        }
+    }
+}
